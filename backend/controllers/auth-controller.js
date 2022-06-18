5973e7816b1fe3bd556eedd24c0fb4a4fbb7d832
Praveen <spraveen593@gmail.com>
2022-06-18 22:24:52 +0530

    Added: My User Profile Route

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,38 +1,91 @@
 import axios from "axios";
 import User from "../models/login.js";
 import Alive from "../services/alive-services.js";
+import { generateAuthToken } from "../services/token-services.js";
+
+// @desc    - Login with ERP
+// @route   POST /api/auth/login
+// @access  Public
+
 const login = async (req, res) => {
   const { auid, password } = req.body;
-  const { data } = await axios({
-    url: process.env.ERP_LOGIN,
-    method: "POST",
-    data: {
-      username: auid,
-      password: password,
-      usertype: "STUDENT",
-    },
-  });
-  if (!data.success) {
-    return res.json({
-      success: false,
-      message: "Invalid Credentials",
+  try {
+    const { data } = await axios({
+      url: process.env.ERP_LOGIN,
+      method: "POST",
+      data: {
+        username: auid,
+        password: password,
+        usertype: "STUDENT",
+      },
     });
-  } else {
-    const isExist = await User.findOne({ auid: auid });
-    if (!isExist) {
-      const user = new User({
-        auid: auid,
-        role: "STUDENT",
+    if (!data.success) {
+      return res.json({
+        success: false,
+        message: "Invalid Credentials",
       });
-      await user.save();
+    } else {
+      var userId;
+      const isExist = await User.findOne({ auid: auid });
+      if (!isExist) {
+        const user = new User({
+          auid: auid,
+          role: "STUDENT",
+        });
+        await user.save();
+        userId = user._id;
+      }
+      return res.status(200).json({
+        success: true,
+        message: "Successfully Logged in",
+        token: data.token,
+        aliveToken: await Alive.aliveLogin(auid, password),
+        Oauth: generateAuthToken(userId ? userId : isExist._id),
+      });
     }
-    return res.status(200).json({
-      success: true,
-      message: "Successfully Logged in",
-      token: data.token,
-      aliveToken: await Alive.aliveLogin(auid, password),
+  } catch (error) {
+    return res.status(404).json({
+      success: false,
+      message: "Server Error, Please try again later.",
     });
   }
 };
 
-export { login };
+// @desc    - User Profile
+// @route   GET /api/auth/profile
+// @access  Public
+
+const profile = async (req, res) => {
+  try {
+    const { data } = await axios({
+      url: process.env.ERP_PROFILE,
+      method: "POST",
+      headers: {
+        token: req.headers.token,
+      },
+    });
+    if (!data.success) {
+      return res.status(201).json({
+        success: false,
+        message: "Unable to retreive Student Information",
+      });
+    } else {
+      const checkStatus = await User.findOne({ auid: data.data.auid });
+      return res.status(200).json({
+        success: true,
+        message: "User data retreive successfully",
+        data: {
+          ...data.data,
+          role: checkStatus.role,
+        },
+      });
+    }
+  } catch (error) {
+    return res.status(404).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+export { login, profile };
--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { login } from "../controllers/auth-controller.js";
+import { login, profile } from "../controllers/auth-controller.js";
 const router = express.Router();
 
 router.route("/login").post(login);
+router.route("/profile").get(profile);
 export default router;
