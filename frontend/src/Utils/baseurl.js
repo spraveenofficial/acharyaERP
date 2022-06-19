2f1633f38a827e6fd92d2b363c64fffc18e7d5d8
Praveen <spraveen593@gmail.com>
2022-06-20 00:39:51 +0530

    Added: Deployed API

--- frontend/src/Utils/baseurl.js
+++ frontend/src/Utils/baseurl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.REACT_APP_MODE === "dev"
     ? "http://localhost:4001/v1/api"
-    : "http://localhost:4001/v1/api";
+    : "https://acharya-erp.vercel.app/v1/api";
 
 export default baseUrl;
